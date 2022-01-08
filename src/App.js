8bee4a6c5adbb3f00fdbe75a0201ab68199425a6
Laura Suarez <laurav.suarezg@utadeo.edu.co>
2022-01-08 13:57:20 -0500

    Changed in the class names of Header component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 //Components
 import Banner from './components/Banner';
 import Header from './components/Header';
+import Instruction from './components/Instruction';
 
 
 const App=()=> {
   return (
     <>
       <Header />
-      <Banner/>
+      <Banner />
+      <Instruction/>
     </>
   );
 }
--- src/components/Banner.js
+++ src/components/Banner.js
@@ -2,24 +2,24 @@ const Banner = () => {
   return (
     <div className="banner">
       <div className="container">
-        <div className="banner-container-information">
-          <div className="banner-information">
-            <p className="banner-question-opinion">What’s your opinion on</p>
-            <h2 className="banner-person-name">Pope Francis?</h2>
-            <p className="banner-person-description">
+        <div className="featured-card">
+          <div className="featured-card-information">
+            <p className="featured-card-question-opinion">What’s your opinion on</p>
+            <h2 className="featured-card-person-name">Pope Francis?</h2>
+            <p className="featured-card-person-description">
               He’s talking tough on clergy sexual abuse, or is he just another pervert protector? (thumbs down) or a true pedophile punishing pontiff? (thumbs up)
             </p>
-            <p className="banner-question-veredict">What’s Your Veredict?</p>
+            <p className="featured-card-question-veredict">What’s Your Veredict?</p>
           </div>
 
-          <div className="banner-thumbs">
+          <div className="featured-card-thumbs">
             <span className="icon-thumb-up" />
             <span className="icon-thumb-down" />
           </div>
         </div>
 
       </div>
-      <div className="banner-time-remainder">
+      <div className="closing-in-bar">
         <div>closing in</div>
         <div><span>22</span> days</div>
       </div>
