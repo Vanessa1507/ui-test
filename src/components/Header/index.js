a3e8e3534e84435523319d20e8ab6faa1e08e05f
Laura Suarez <laurav.suarezg@utadeo.edu.co>
2022-01-09 10:01:22 -0500

    Functionality mobile menu

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,8 +1,19 @@
+import { useState } from 'react';
 //Hooks
 import useIsMobile from '../../hooks/useIsMobile';
 
+const elementsMenu = [
+  { name: 'Past Trials', link: '#' },
+  { name: 'How It Works', link: '#' },
+  { name: 'Login / Sign Up', link: '#' }
+]
+
 const Header = () => {
+  const [isShowMenu, setIsShowMenu] = useState(true);
   const isMobile = useIsMobile();
+
+  const closeOpenMenu = (value) => setIsShowMenu(value);
+
 
   return (
     <header>
@@ -10,9 +21,36 @@ const Header = () => {
         <h1>Rule of thumb.</h1>
         {
           isMobile && (
-            <button>
-              <span className='icon-menu' />
-            </button>
+            <>
+              <button onClick={(e) => closeOpenMenu(e, true)}>
+                <span className='icon-menu' />
+              </button>
+              {
+                isShowMenu && (
+                  <>
+                    <div className='mobile-menu-background' onClick={() => closeOpenMenu(false)} />
+                    <ul className='mobile-menu'>
+                      <li>
+                        <form className='mobile-menu-search' onSubmit={e => e.preventDefault()}>
+                          <input type='text' />
+                          <button>
+                            <span className='icon-search' />
+                          </button>
+                        </form>
+                      </li>
+                      {
+                        !!elementsMenu && elementsMenu.length &&
+                        elementsMenu.map(link => (
+                          <li onClick={() => closeOpenMenu(false)} >
+                            <a href={link.link}>{link.name}</a>
+                          </li>
+                        ))
+                      }
+                    </ul>
+                  </>
+                )
+              }
+            </>
           )
         }
         {
