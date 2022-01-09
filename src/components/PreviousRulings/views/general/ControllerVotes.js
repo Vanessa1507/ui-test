b54dedc8302419da1eb984e27921b26bb2bf6919
Laura Suarez <laurav.suarezg@utadeo.edu.co>
2022-01-08 21:52:19 -0500

    I added the styles and function when I select the button thumb-up or down

--- src/components/PreviousRulings/views/general/ControllerVotes.js
+++ src/components/PreviousRulings/views/general/ControllerVotes.js
@@ -1,13 +1,31 @@
+import { useState } from "react"
+
 const ControllerVotes = () => {
+
+  const [isDisable, setIsDisable] = useState(true);
+  const [activeThumb, setActiveThumb] = useState(null);
+
+  const buttons = ['up', 'down'];
+
+
+  const changeActiveThumb = (value) => {
+    setIsDisable(false);
+    setActiveThumb(value);
+  };
+
+
+
   return (
     <div className="view-grid-controller-votes">
-      <button>
-        <span className="icon-thumb icon-thumb-up" />
-      </button>
-      <button>
-        <span className="icon-thumb icon-thumb-down" />
-      </button>
-      <button className="button-vote">
+      {
+        buttons.map(button => (
+          <button onClick={() => { changeActiveThumb(button) }}>
+            <span className={`icon-thumb icon-thumb-${button} ${button === activeThumb ? 'active-thumb' : ''}`} />
+          </button>
+        ))
+      }
+
+      <button className="button-vote" disabled={isDisable}>
         Vote Now
       </button>
     </div>
